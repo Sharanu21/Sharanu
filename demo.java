66561422fc9005f3b654cd317f37208113a848fe
Sharanu21 <sharanumeti53@gmail.com>
2025-05-15 14:59:54 +0530

    added commi with README AND JAVA file

---
+++ demo.java
@@ -0,0 +1,9 @@
+public clpublic class HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    }
+}ass HelloGitHub {
+    public static void main(String[] args) {
+        System.out.println("Hello, GitHub!");
+    }
+}
